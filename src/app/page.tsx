85c3056dd0be42ffebcdd4797b22d340b4ebea9c
Aleksandr <smirnow-2012@mail.ru>
2023-10-20 23:01:57 +0300

    add Button component and fix stylelint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import { Htag } from '@/components';
+import { Button, Htag } from '@/components';
 import styles from './page.module.css';
 import { Metadata } from 'next';
-
 
 export async function generateMetadata(): Promise<Metadata> {
 	return {
@@ -14,6 +13,8 @@ export default function Home() {
 		<main className={styles.main}>
 			<div className={styles.description}>
 				<Htag tag="h1">Текст</Htag>
+				<Button appearance="primary">Сохранить</Button>
+				<Button appearance="ghost">Сохранить</Button>
 			</div>
 		</main>
 	);
