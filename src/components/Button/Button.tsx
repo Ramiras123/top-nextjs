291c2ce463a2db3b1df64212c04d135e1a811a2e
Aleksandr <smirnow-2012@mail.ru>
2023-10-20 23:26:09 +0300

    fix

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,13 +2,19 @@ import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
 import cn from 'classnames';
 
-export const Button = ({ appearance, children }: ButtonProps) => {
+export const Button = ({
+	appearance,
+	children,
+	className,
+	...props
+}: ButtonProps) => {
 	return (
 		<button
-			className={cn(styles['button'], {
+			className={cn(styles['button'], className, {
 				[styles['primary']]: appearance === 'primary',
 				[styles['ghost']]: appearance === 'ghost'
 			})}
+			{...props}
 		>
 			{children}
 		</button>
