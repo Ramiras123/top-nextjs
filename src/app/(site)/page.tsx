122bccc380cb1701d94d3f959aa1dc6a28ea2f5f
Aleksandr <smirnow-2012@mail.ru>
2023-10-30 16:19:04 +0300

    add textarea component

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,4 +1,4 @@
-import { Button, Htag, Input, Paragraph, Tag } from '@/components';
+import { Button, Htag, Input, Paragraph, Tag, Textarea } from '@/components';
 import styles from './page.module.css';
 import { Metadata } from 'next';
 import { ClientRatingComponent } from '@/ClientComponent';
@@ -43,6 +43,7 @@ export default async function Home() {
 				</Tag>
 				<ClientRatingComponent />
 				<Input placeholder="Имя" />
+				<Textarea />
 			</div>
 		</main>
 	);
