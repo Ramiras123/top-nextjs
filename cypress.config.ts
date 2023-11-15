400b057b33c1ccf44802bb813ca2df29aa7cfb5c
Aleksandr <smirnow-2012@mail.ru>
2023-11-15 19:13:18 +0300

    init cypress

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "ge46bm",
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
